133c1ef0a447ee44535666511c1200f2bdb022fc
zawzawzaw <zawzawzaw@gmail.com>
2014-12-09 13:05:23 +0800

    updated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-	
+	$("#jquery_jplayer_1").jPlayer({
+		ready: function () {
+			$(this).jPlayer("setMedia", {
+				title: "Mitzo",
+				m4v: "http://clients.manic.com.sg/mitzo/html/videos/mitzo.mp4", //http://www.jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v
+				poster: "images/content/cover-video-img.png"
+			});
+		},
+		swfPath: "jplayer",
+		supplied: "webmv, ogv, m4v",
+		size: {
+			width: "960px",
+			height: "540px",
+			cssClass: "jp-video-270p"
+		},
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true,
+		remainingDuration: true,
+		toggleDuration: true
+	});
+
+	$('a.watch').fancybox({
+        padding: 10,
+        autoDimensions: false,
+        closeBtn : false,
+        scrolling : 'no',
+        afterShow: function () {
+        	$("#jquery_jplayer_1").jPlayer("stop");
+        	$("#jquery_jplayer_1").jPlayer("play");
+        }
+    });
 });
